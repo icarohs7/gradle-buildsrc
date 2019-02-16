439ca7d5dc0f0cd2a38b27e889187399107fec13
Icaro Temponi <icarohs7@gmail.com>
2019-02-15 22:07:09 -0200

    auto commit push on 15/02/2019 22:07

--- src/main/java/Dependencies.kt
+++ src/main/java/Dependencies.kt
@@ -305,7 +305,7 @@ object Versions {
     const val timber = "4.7.1"
     const val tornadoFx = "1.7.17"
     const val unoxAndroid = "2.4.8"
-    const val unoxAndroidArch = "0.22"
+    const val unoxAndroidArch = "e52b31bfc2"
 
     const val barista = "2.7.1"
     const val fixd = "1.0.3"
