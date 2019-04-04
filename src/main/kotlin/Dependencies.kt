a2644c35b84f9fac2fa3a36ef0153a5d143acc91
Icaro Temponi <icarohs7@gmail.com>
2019-04-04 08:55:44 -0300

    auto commit push on 04/04/2019 08:55

--- src/main/kotlin/Dependencies.kt
+++ src/main/kotlin/Dependencies.kt
@@ -361,7 +361,7 @@ object Versions {
     const val stickyTimelineView = "v0.0.20"
     const val timber = "4.7.1"
     const val tornadoFx = "1.7.17"
-    const val unoxAndroidArch = "f7cab1cff7"
+    const val unoxAndroidArch = "9afdde32b5"
     const val unoxCore = "3.00-next.1"
     const val viewAnimator = "1.1.1"
     const val workManager = "2.0.0"
