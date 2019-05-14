fff3074f3ad59710ff36b44501aff2ebd2b41b43
Icaro Temponi <icarohs7@gmail.com>
2019-05-14 15:39:30 -0300

    auto commit push on 14/05/2019 15:39

--- src/main/kotlin/defaults/root-module.gradle.kts
+++ src/main/kotlin/defaults/root-module.gradle.kts
@@ -3,10 +3,10 @@ package defaults
 allprojects {
     repositories {
         google()
+        mavenLocal()
         mavenCentral()
         maven("http://devrepo.kakao.com:8088/nexus/content/groups/public/")
         maven("https://kotlin.bintray.com/kotlinx")
-        maven("https://dl.bintray.com/icarohs7/libraries")
         maven("https://jitpack.io")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
         jcenter()
