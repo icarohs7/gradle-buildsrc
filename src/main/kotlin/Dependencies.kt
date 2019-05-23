0592932591a6a4e729729b37142881a2c0d63fc9
Icaro Temponi <icarohs7@gmail.com>
2019-05-23 13:47:29 -0300

    auto commit push on 23/05/2019 13:47

--- src/main/kotlin/Dependencies.kt
+++ src/main/kotlin/Dependencies.kt
@@ -376,7 +376,7 @@ object Versions {
     const val textViewDotsAnimation = "v1.0"
     const val timber = "4.7.1"
     const val tornadoFx = "1.7.17"
-    const val unoxAndroidArch = "0.50-next.14"
+    const val unoxAndroidArch = "0.51-next.1"
     const val unoxCore = "3.02-next.5"
     const val viewAnimator = "1.1.1"
     const val workManager = "2.0.1"
