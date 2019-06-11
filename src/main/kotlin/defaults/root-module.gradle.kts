98b18971489d1d041ff0b5f38600f046b116cda0
Icaro Temponi <icarohs7@gmail.com>
2019-06-11 17:57:54 -0300

    auto commit push on 11/06/2019 17:57

--- src/main/kotlin/defaults/root-module.gradle.kts
+++ src/main/kotlin/defaults/root-module.gradle.kts
@@ -8,6 +8,7 @@ allprojects {
         maven("http://devrepo.kakao.com:8088/nexus/content/groups/public/")
         maven("https://kotlin.bintray.com/kotlinx")
         maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        maven("https://dl.bintray.com/icarohs7/libraries")
         jcenter()
         maven("https://jitpack.io")
     }
