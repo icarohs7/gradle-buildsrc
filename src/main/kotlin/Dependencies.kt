33a02b22d80179ddf3027926a52e6f4a177ec663
Icaro Temponi <icarohs7@gmail.com>
2019-07-25 11:28:35 -0300

    auto commit push on 25/07/2019 11:28

--- src/main/kotlin/Dependencies.kt
+++ src/main/kotlin/Dependencies.kt
@@ -316,7 +316,7 @@ object Versions {
     const val circularImageView = "1.4"
     const val clikt = "1.7.0"
     const val constraintLayout = "2.0.0-beta2"
-    const val coroutines = "1.2.2"
+    const val coroutines = "1.3.0-RC"
     const val crunchyCalendar = "2.0.0"
     const val cryptoPrefs = "1.3.2.5"
     const val currencyEditText = "2.0.2"
