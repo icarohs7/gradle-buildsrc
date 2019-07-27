c72be11433c90df19fee9f71e1d1678c2b4fff52
Icaro Temponi <icarohs7@gmail.com>
2019-07-27 18:29:47 -0300

    auto commit push on 27/07/2019 18:29

--- src/main/kotlin/DependenciesExtensions.kt
+++
@@ -1,7 +0,0 @@
-import org.gradle.api.artifacts.ProjectDependency
-import org.gradle.kotlin.dsl.DependencyHandlerScope
-import org.gradle.kotlin.dsl.project
-
-fun DependencyHandlerScope.res(resModuleName: String): ProjectDependency {
-    return project(":resmodules:${resModuleName}resources")
-}
--- src/main/kotlin/KotlinCompileExtensions.kt
+++
@@ -1,7 +0,0 @@
-import org.gradle.api.Project
-import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinCompilation
-
-fun AbstractKotlinCompilation<*>.setupMetaInfoName(rootProject: Project, project: Project) {
-    val fileName = "${rootProject.name}${project.path}".replace(":", "_")
-    kotlinOptions.freeCompilerArgs += listOf("-module-name", fileName)
-}
