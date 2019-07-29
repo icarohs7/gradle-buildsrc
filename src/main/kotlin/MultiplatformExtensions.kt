412acb9987684819ac43554c0a94aec1e025e42e
Icaro Temponi <icarohs7@gmail.com>
2019-07-29 10:34:47 -0300

    Revert "auto commit push on 27/07/2019 18:29"
    Signed-off-by: Icaro Temponi <icarohs7@gmail.com>

---
+++ src/main/kotlin/MultiplatformExtensions.kt
@@ -0,0 +1,35 @@
+import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
+import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinTarget
+
+fun KotlinMultiplatformExtension.setupMetaInfoNameOnAll(rootProject: Project, project: Project) {
+    targets.all {
+        compilations.all {
+            val fileName = "${rootProject.name}${project.path}".replace(":", "_")
+            kotlinOptions.freeCompilerArgs += listOf("-module-name", fileName)
+        }
+    }
+}
+
+fun KotlinMultiplatformExtension.setupMetadataTarget(rootProject: Project, project: Project, artifactId: String) {
+    metadata { mavenPublication { this.artifactId = artifactId } }
+}
+
+fun KotlinMultiplatformExtension.setupAndroidTarget(
+        rootProject: Project,
+        project: Project,
+        artifactId: String
+) {
+    android {
+        mavenPublication { this.artifactId = artifactId }
+        publishLibraryVariants("debug")
+        compilations.all { kotlinOptions.jvmTarget = "1.8" }
+    }
+}
+
+fun AbstractKotlinTarget.setupMetaInfoName(rootProject: Project, project: Project) {
+    compilations.all {
+        val fileName = "${rootProject.name}${project.path}".replace(":", "_")
+        kotlinOptions.freeCompilerArgs += listOf("-module-name", fileName)
+    }
+}
