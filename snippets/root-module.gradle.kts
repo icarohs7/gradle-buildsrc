7e4709a81dfc24157623566160a57ff9bb15ac63
Icaro Temponi <icarohs7@gmail.com>
2019-07-29 19:14:54 -0300

    auto commit push on 29/07/2019 19:14

--- snippets/root-module.gradle.kts
+++ snippets/root-module.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     defaults.`root-module`
     id("com.github.ben-manes.versions") version "0.21.0"
+    id("com.vanniktech.dependency.graph.generator") version "0.5.0"
 }
 
 tasks.create("genDatabindingClassesOnAllModules") {
