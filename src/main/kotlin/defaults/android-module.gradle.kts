e3cea94cba1f8d65c116682284808fbd4d7a6d11
Icaro Temponi <icarohs7@gmail.com>
2019-08-07 16:44:12 -0300

    auto commit push on 07/08/2019 16:44

--- src/main/kotlin/defaults/android-module.gradle.kts
+++ src/main/kotlin/defaults/android-module.gradle.kts
@@ -1,16 +1,15 @@
 package defaults
 
-import AndroidTestDeps
 import TestDeps
 
 dependencies {
     val add = { type: String, dependencies: List<String> -> dependencies.forEach { add(type, it) } }
 
     add("testImplementation", TestDeps.androidCore)
-    add("androidTestImplementation", AndroidTestDeps.core)
-    add("androidTestUtil", AndroidTestDeps.testOrchestrator)
-    add("androidTestImplementation", AndroidTestDeps.barista) {
-        exclude(group = "com.android.support")
-        exclude(group = "org.jetbrains.kotlin")
-    }
+//    add("androidTestImplementation", AndroidTestDeps.core)
+//    add("androidTestUtil", AndroidTestDeps.testOrchestrator)
+//    add("androidTestImplementation", AndroidTestDeps.barista) {
+//        exclude(group = "com.android.support")
+//        exclude(group = "org.jetbrains.kotlin")
+//    }
 }
