d6ba4c0938a4f716140f5db12e05bc857a427c90
Icaro Temponi <icarohs7@gmail.com>
2019-08-10 00:33:49 -0300

    auto commit push on 10/08/2019 00:33

--- src/main/kotlin/KotlinCompileExtensions.kt
+++ src/main/kotlin/KotlinCompileExtensions.kt
@@ -1,7 +1,15 @@
 import org.gradle.api.Project
+import org.jetbrains.kotlin.gradle.dsl.KotlinCompile
 import org.jetbrains.kotlin.gradle.plugin.mpp.AbstractKotlinCompilation
 
 fun AbstractKotlinCompilation<*>.setupMetaInfoName(rootProject: Project, project: Project) {
     val fileName = "${rootProject.name}${project.path}".replace(":", "_")
     kotlinOptions.freeCompilerArgs += listOf("-module-name", fileName)
+}
+
+fun KotlinCompile<*>.useExperimentalFeatures(usedExperimentalMarkers: List<String> = emptyList()) {
+    kotlinOptions.freeCompilerArgs += listOf(
+            "-Xuse-experimental=kotlin.Experimental",
+            "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
+    ) + usedExperimentalMarkers
 }
--- src/main/kotlin/ProjectExtensions.kt
+++ src/main/kotlin/ProjectExtensions.kt
@@ -1,17 +1,5 @@
 import org.gradle.api.Project
-import org.gradle.jvm.tasks.Jar
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun Project.findProp(s: String) = project.findProperty(s) as String
 
 fun Project.nameWithPath(rootProject: Project): String = "${rootProject.name}$path"
-
-fun Project.useExperimentalFeatures(usedExperimentalMarkers: List<String> = emptyList()) {
-    tasks.withType<KotlinCompile>().all {
-        kotlinOptions.freeCompilerArgs += listOf(
-                "-Xuse-experimental=kotlin.Experimental",
-                "-Xuse-experimental=kotlinx.coroutines.ExperimentalCoroutinesApi"
-        ) + usedExperimentalMarkers
-    }
-}
