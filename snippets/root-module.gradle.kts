64ae4c7c97bf3dfd09aab42774fcd4e48126b9b0
Icaro Temponi <icarohs7@gmail.com>
2019-08-13 21:47:36 -0300

    auto commit push on 13/08/2019 21:47

--- snippets/root-module.gradle.kts
+++ snippets/root-module.gradle.kts
@@ -1,11 +1,5 @@
 plugins {
     defaults.`root-module`
-    id("com.github.ben-manes.versions") version "0.21.0"
-    id("com.vanniktech.dependency.graph.generator") version "0.5.0"
-}
-
-tasks.create("genDatabindingClassesOnAllModules") {
-    getTasksByName("dataBindingGenBaseClassesDebug", true).forEach { task ->
-        dependsOn(task)
-    }
+    id("com.github.ben-manes.versions")
+    id("com.vanniktech.dependency.graph.generator")
 }
