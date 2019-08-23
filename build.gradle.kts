d4ad826e98f513f04c24c1d452a1a79010164878
Icaro Temponi <icarohs7@gmail.com>
2019-08-23 08:35:06 -0300

    auto commit push on 23/08/2019 08:35

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     val gradlePluginVersion = "3.5.0"
-    val kotlinVersion = "1.3.41"
+    val kotlinVersion = "1.3.50"
     val navigationVersion = "2.2.0-alpha01"
     val googleServicesVersion = "4.2.0"
     val bintrayPluginVersion = "1.8.4"
