2a2736a31af79f5bbe84b88aa5310c1544c1c5df
Icaro Temponi <icarohs7@gmail.com>
2019-09-03 10:34:43 -0300

    auto commit push on 03/09/2019 10:34

--- src/main/kotlin/DependenciesExtensions.kt
+++ src/main/kotlin/DependenciesExtensions.kt
@@ -1,7 +1,11 @@
+import org.gradle.api.Project
 import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.project
 
-fun DependencyHandlerScope.res(resModuleName: String): ProjectDependency {
-    return project(":resmodules:${resModuleName}resources")
+fun DependencyHandlerScope.res(resModuleName: String, project: Project? = null): ProjectDependency {
+    return if (project?.hasProperty("relativeResMod") == true)
+        project(":${resModuleName}resources")
+    else
+        project(":resmodules:${resModuleName}resources")
 }
