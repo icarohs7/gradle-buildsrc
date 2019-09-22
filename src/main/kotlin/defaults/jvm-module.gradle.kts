0da3b34a02d6d85d05ac8fa4b1435fe936175c62
Icaro Temponi <icarohs7@gmail.com>
2019-09-21 21:39:58 -0300

    auto commit push on 21/09/2019 21:39

--- src/main/kotlin/defaults/jvm-module.gradle.kts
+++ src/main/kotlin/defaults/jvm-module.gradle.kts
@@ -1,6 +1,15 @@
 package defaults
 
 import TestDeps
+import compileKotlin
+import useExperimentalFeatures
+
+compileKotlin {
+    useExperimentalFeatures()
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
 
 dependencies {
     val add = { type: String, dependencies: List<String> -> dependencies.forEach { add(type, it) } }
