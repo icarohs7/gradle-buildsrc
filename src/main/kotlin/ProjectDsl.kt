f62726427a093bb89e532bf6b796f18d6c8a4467
Icaro Temponi <icarohs7@gmail.com>
2019-10-02 21:29:34 -0300

    auto commit push on 02/10/2019 21:29

--- src/main/kotlin/ProjectDsl.kt
+++ src/main/kotlin/ProjectDsl.kt
@@ -1,12 +1,12 @@
 import org.gradle.api.Project
 import org.gradle.jvm.tasks.Jar
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 fun Project.jar(block: Jar.() -> Unit) {
     tasks.withType<Jar>().all(block)
 }
 
 fun Project.compileKotlin(block: KotlinCompile.() -> Unit) {
-    tasks.withType<KotlinCompile>(block)
+    tasks.withType(block)
 }
