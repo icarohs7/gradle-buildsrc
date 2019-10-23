9e93d4e2e15e48a5be7ef17cc025ed35c5379e9e
Icaro Temponi <icarohs7@gmail.com>
2019-10-23 15:08:32 -0300

    auto commit push on 23/10/2019 15:08

--- src/main/kotlin/Extensions.kt
+++ src/main/kotlin/Extensions.kt
@@ -1,6 +1,9 @@
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.DependencyHandlerScope
 import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.withGroovyBuilder
 import java.io.File
+import java.util.Properties
 import com.android.build.gradle.AppExtension as AndroidApplicationBlock
 import com.android.build.gradle.TestedExtension as AndroidBlock
 
@@ -34,5 +37,47 @@ fun AndroidApplicationBlock.enableProguard() {
             isShrinkResources = true
             proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
         }
+    }
+}
+
+fun readPropertiesFile(file: File): Properties {
+    return Properties().apply {
+        load(file.reader(Charsets.UTF_8))
+    }
+}
+
+fun Project.setupFlutterAndroidModuleSettings(localProperties: Properties) {
+    val flutterVersionCode = localProperties.getProperty("flutter.versionCode") ?: "1"
+    val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0"
+
+    extensions.configure<AndroidBlock>("android") {
+        compileSdkVersion(Versions.compileSdk)
+        sourceSets["main"].java.srcDir("src/main/kotlin")
+        sourceSets["test"].java.srcDir("src/test/kotlin")
+
+        lintOptions {
+            disable("InvalidPackage")
+        }
+
+        defaultConfig {
+            minSdkVersion(Versions.minSdk)
+            targetSdkVersion(Versions.targetSdk)
+            versionCode = flutterVersionCode.toInt()
+            versionName = flutterVersionName
+            testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        }
+    }
+
+    withGroovyBuilder {
+        "flutter" {
+            setProperty("source", "../..")
+        }
+    }
+
+    DependencyHandlerScope.of(dependencies).apply {
+        "implementation"(Deps.kotlinStdLib)
+        "testImplementation"(TestDeps.junit)
+        "androidTestImplementation"(AndroidTestDeps.testRunner)
+        "androidTestImplementation"(AndroidTestDeps.espressoCore)
     }
 }
